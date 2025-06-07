496247ce8767ecbe82b81dd9884949bcdf8253a2
Oleg579 <olegkozachenko98@gmail.com>
2025-06-07 18:04:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -147,3 +147,8 @@ if __name__ == "__main__":
             import uvicorn
 
             uvicorn.run(app, host="0.0.0.0", port=8000)
+
+            if __name__ == "__main__":
+                import uvicorn
+
+                uvicorn.run(app, host="0.0.0.0", port=8000)
